3405fb8cf1f9e65f17fba46ebabafb19e18331c4
Sean M. Sullivan <sean@barriquesoft.com>
2014-05-08 13:34:43 -0700

    Added comments to better document class.

--- esutil/connection.py
+++ esutil/connection.py
@@ -8,8 +8,13 @@ class Connection(object):
     def __init__(self, host_name, port_num):
         """
         Instantiate the ElasticSearch connection object.
+            host_name       Host name of an ElasticSearch node
+            port_number     Port number of the ElasticSearch API on node
         """
         self.es_host = "%s:%s" % (host_name, port_num)
 
     def get_connection(self):
+        """
+        Returns an ElasticSearch connection object.
+        """
         return Elasticsearch(self.es_host)
